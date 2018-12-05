56ea162ce1e4623630a6e1bd52ac3b05d379aa6a
arvind <arvind.jatav@diaspark.com>
2018-12-05 14:46:06 +0530

    intial commit against sm ci/cd evaluation.

---
+++ sm-app/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sm_app.Global" Language="C#" %>
---
+++ sm-app/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sm_app.Startup))]
+namespace sm_app
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
